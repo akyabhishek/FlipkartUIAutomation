b65d6b1e50dd63866c485143efb2a97d25215bdd
Abhishek Kumar Yadav <37768757+akyabhishek@users.noreply.github.com>
2023-06-17 19:03:34 +0530

    1.0.0

--- src/main/java/utils/LocalTextFormatter.java
+++ src/main/java/utils/LocalTextFormatter.java
@@ -3,7 +3,7 @@ package utils;
 public class LocalTextFormatter {
 	// converts and returns the integer value of price
 	public static int formatPrice(String price) {
-		return Integer.valueOf(price.replaceAll("₹", "").replaceAll(",", ""));
+		return Integer.valueOf(price.replaceAll("₹", "").replaceAll(",", "").replaceAll("?", ""));
 	}
 
 }
